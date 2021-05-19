17b6bce58979414a0e948aa77f163dcaaa7e0ef6
Eduardo Brito <alu0100783315@ull.edu.es>
2021-05-19 10:30:21 +0100

    Delete and update working

---
+++ src/delete.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+import {User} from './helpers';
+
+mongoose.connect('mongodb://127.0.0.1:27017/dsi-assignment', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => {
+  User.deleteOne({email: 'alu0100783315@ull.edu.es'}).then((result) => {
+    console.log(result.deletedCount);
+  }).catch((error) => {
+    console.log(error);
+  });
+}).catch(() => {
+  console.log('Something went wrong when conecting to the database');
+});
---
+++ src/update.ts
@@ -0,0 +1,16 @@
+import * as mongoose from 'mongoose';
+import {User} from './helpers';
+
+mongoose.connect('mongodb://127.0.0.1:27017/dsi-assignment', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => {
+  User.updateOne({email: 'eebritosa@gmail.com'}).then((result) => {
+    console.log(result.nModified);
+  }).catch((error) => {
+    console.log(error);
+  });
+}).catch(() => {
+  console.log('Something went wrong when conecting to the database');
+});
